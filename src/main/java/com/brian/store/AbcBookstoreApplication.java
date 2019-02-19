f9c5bbb29dcc24c5be91ee694164e6b42ca0578c
briancollins081 <briancollins081@gmail.com>
2019-02-19 04:28:05 +0300

    created static home page and accounts page

---
+++ src/main/java/com/brian/store/AbcBookstoreApplication.java
@@ -0,0 +1,14 @@
+package com.brian.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcBookstoreApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/brian/store/domain/User.java
@@ -0,0 +1,13 @@
+package com.brian.store.domain;
+
+public class User {
+	private Long id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	
+	private String email;
+	private String phone;
+	private boolean enable=true;
+}
