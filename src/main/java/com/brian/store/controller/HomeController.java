52782397401123e5456cbf06dc688a64f737057d
briancollins081 <briancollins081@gmail.com>
2019-03-06 01:31:38 +0300

    Adding on the user billing and user shipping functionality

--- src/main/java/com/brian/store/controller/HomeController.java
+++ src/main/java/com/brian/store/controller/HomeController.java
@@ -28,6 +28,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.brian.store.domain.Book;
 import com.brian.store.domain.User;
+import com.brian.store.domain.UserBilling;
+import com.brian.store.domain.UserPayment;
 import com.brian.store.domain.UserShipping;
 import com.brian.store.domain.security.PasswordResetToken;
 import com.brian.store.domain.security.Role;
@@ -89,12 +91,97 @@ public class HomeController {
 		List<String> stateList=USConstants.listOfUSStatesCode;
 		Collections.sort(stateList);
 		model.addAttribute("stateList",stateList);
-		model.addAttribute("classActivatedEdit",true);
+		model.addAttribute("classActiveEdit",true);
 		
 		
 		return "myProfile";
 	}
+	@RequestMapping("/listOfCreditCards")
+	public String listOfCreditCards(
+			Model model, Principal principal, HttpServletRequest request
+			) {
+		
+		User user=userService.findByUsername(principal.getName());
+		model.addAttribute("user", user);
+		model.addAttribute("userPaymentList",user.getUserPaymentList());
+		model.addAttribute("userShippingList", user.getUserShippingList());
+		//model.addAttribute("orderList", user.orderList());
+		
+		model.addAttribute("listOfCreditCards", true);
+		model.addAttribute("classActiveBilling", true);
+		model.addAttribute("listOfShippingAddresses", true);
+		return "myProfile";
+	}
 	
+	@RequestMapping("/listOfShippingAddresses")
+	public String listOfShippingAddresses(
+			Model model, Principal principal, HttpServletRequest request
+			) {
+		
+		User user=userService.findByUsername(principal.getName());
+		model.addAttribute("user", user);
+		model.addAttribute("userPaymentList",user.getUserPaymentList());
+		model.addAttribute("userShippingList", user.getUserShippingList());
+		//model.addAttribute("orderList", user.orderList());
+		
+		model.addAttribute("listOfCreditCards", true);
+		model.addAttribute("classActiveShipping", true);
+		model.addAttribute("listOfShippingAddresses", true);
+		return "myProfile";
+	}
+	
+	@RequestMapping("/addNewCreditCard")
+	public String addNewCreditCard(
+			Model model, Principal principal
+			) {
+		User user=userService.findByUsername(principal.getName());
+		model.addAttribute("user", user);
+		model.addAttribute("addNewCreditCard", true);
+		model.addAttribute("classActiveBilling", true);
+		model.addAttribute("listOfShippingAddresses", true);
+		
+		UserBilling userBilling=new UserBilling();
+		UserPayment userPayment=new UserPayment();
+		
+		model.addAttribute("userBilling", userBilling);
+		model.addAttribute("userPayment", userPayment);
+		
+		List<String> stateList=USConstants.listOfUSStatesCode;
+		Collections.sort(stateList);
+		model.addAttribute("stateList", stateList);
+		
+		model.addAttribute("userPaymentList",user.getUserPaymentList());
+		model.addAttribute("userShippingList", user.getUserShippingList());
+		//model.addAttribute("orderList", user.orderList());
+		
+		return "myProfile";
+	}
+	
+	@RequestMapping("/addNewShippingAddress")
+	public String addNewShippingAddress(
+			Model model, Principal principal
+			) {
+		User user=userService.findByUsername(principal.getName());
+		model.addAttribute("user", user);
+		model.addAttribute("addNewShippingAddress", true);
+		model.addAttribute("classActiveShipping", true);
+		
+		UserShipping userShipping=new UserShipping();
+		
+		model.addAttribute("userShipping", userShipping);
+		
+		List<String> stateList=USConstants.listOfUSStatesCode;
+		Collections.sort(stateList);
+		model.addAttribute("stateList", stateList);
+		
+		model.addAttribute("userPaymentList",user.getUserPaymentList());
+		model.addAttribute("userShippingList", user.getUserShippingList());
+		//model.addAttribute("orderList", user.orderList());
+		
+		return "myProfile";
+	}
+			
+			
 	@RequestMapping("/bookDetail")
 	public String bookDetail(
 			@PathParam("id") Long id, Model model, Principal principal
